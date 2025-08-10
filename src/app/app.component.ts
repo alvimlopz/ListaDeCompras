23a2ee2b63ed8eb3b396c8afd83368e8fe9024ac
Alvaro <alvim.lopez@gmail.com>
2025-08-10 12:04:25 -0300

    iniciando o projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app-lista-de-compras';
+  listaDeCompra! : Array<Item>
+
+  constructor(private listaService: ListaDeCompraService) { }
+}
